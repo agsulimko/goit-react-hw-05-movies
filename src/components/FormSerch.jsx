2933994ea59c3a15c3d49eab8b7cb6b3918ab156
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-09-28 19:49:36 +0200

    s

--- src/components/FormSerch.jsx
+++ src/components/FormSerch.jsx
@@ -1,61 +1,65 @@
-// import React, { useSearchParams } from "react";
+import React, { useState, useSearchParams } from "react";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 const FormSerch = (props) => {
-  // const [searchParams, setSearchParams] = useSearchParams();
-  // console.log(searchParams);
-  // //   const [inputQuery, setInputQuery] = useState("");
-  // //   const handleInputQuery = (e) => {
-  // //     setInputQuery(e.currentTarget.value.toLowerCase());
-  // //   };
-  // //   const handleSubmit = (e) => {
-  // //     e.preventDefault();
-  // //     if (inputQuery.trim() === "") {
-  // //       alert("Enter your request");
-  // //       return;
-  // //     }
-  // //     props.onSubmit(inputQuery.trim());
-  // //     setInputQuery("");
-  // //   };
-  // //   console.log(inputQuery);
-  // return (
-  <div>
-    <form className="form" onSubmit={handleSubmit}>
-      {/* // <form className="form" onSubmit={handleSubmit}> */}
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <TextField
-          //  className="input"
-          type="text"
-          //  autocomplete="off"
-          //  autofocus
-          //  placeholder="Search images and photos"
-          name="title"
-          size="small"
-          sx={{ m: 1, width: "35ch" }}
-          style={{ backgroundColor: "white" }}
-          className="form-control"
-          onChange={handleInputQuery}
-          //   value={inputQuery}
-          id="input-with-sx"
-          label="Search movies"
-          variant="outlined"
-          margin="dense"
-        />
-      </Box>
-      <button onClick={() => setSearchParams({ c: "HELLO" })}>Button</button>
-      {/* <input
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams);
+  const [inputQuery, setInputQuery] = useState("");
+  const handleInputQuery = (e) => {
+    setInputQuery(e.currentTarget.value.toLowerCase());
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputQuery.trim() === "") {
+      alert("Enter your request");
+      return;
+    }
+    //      props.onSubmit(inputQuery.trim());
+    // //     setInputQuery("");
+    // //   };
+    console.log(inputQuery);
+    return (
+      <div>
+        <form className="form" onSubmit={handleSubmit}>
+          {/* // <form className="form" onSubmit={handleSubmit}> */}
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <TextField
+              //  className="input"
+              type="text"
+              //  autocomplete="off"
+              //  autofocus
+              //  placeholder="Search images and photos"
+              name="title"
+              size="small"
+              sx={{ m: 1, width: "35ch" }}
+              style={{ backgroundColor: "white" }}
+              className="form-control"
+              onChange={handleInputQuery}
+              //   value={inputQuery}
+              id="input-with-sx"
+              label="Search movies"
+              variant="outlined"
+              margin="dense"
+            />
+          </Box>
+          <button onClick={() => setSearchParams({ c: "HELLO" })}>
+            Button
+          </button>
+          {/* <input
         type="email"
         class="form-control"
         id="exampleInputEmail1"
         aria-describedby="emailHelp"
       ></input> */}
-    </form>
-  </div>;
+        </form>
+      </div>
+    );
+  };
 };
 export default FormSerch;
