27b975ed284406d800117fbee994cf845d87cf3c
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-09-28 20:10:13 +0200

    s

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,25 +1,45 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { getAllMovies } from "../api/api";
 import { Link } from "react-router-dom";
-
-const array = ["   a   ", "  b  ", "  c  ", "  d ", "  e  "];
+import css from "./Movies.module.css";
+import FormSerch from "components/FormSerch";
 const Movies = (prev) => {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
   getAllMovies();
-  // useEffect(() => { }, []);
-  //   console.log(mov);
+  const fetchMovies = async () => {
+    try {
+      const { results } = await getAllMovies();
+      //   console.log(results);
+      setMovies((prevMovies) => [...results]);
+    } catch (err) {
+      setError(error.message);
+    }
+  };
+  //   console.log(movies);
+  useEffect(() => {
+    fetchMovies();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <div>
-      Movies страница
-      {array.map((mov, index) => {
-        return (
-          <Link key={index} to={`${mov.id}`}>
-            {mov.id}
-          </Link>
-        );
-      })}
+    <div className="">
+      <FormSerch />
+      {/* <div className={css.homeMovies}> */}
+      <p>Movies страница</p>
+      <ul className={css.listMovies}>
+        {movies.map((mov, index) => {
+          // console.log(mov.id);
+          // console.log(index);
+          return (
+            <li>
+              <Link key={index} to={`${mov.id}`}>
+                {mov.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
-
-//
 export default Movies;
