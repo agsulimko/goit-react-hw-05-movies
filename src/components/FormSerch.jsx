dc4f0d8411f815a55b4d1abb3559fa203d89fcc8
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-09-28 19:44:42 +0200

    s

--- src/components/FormSerch.jsx
+++ src/components/FormSerch.jsx
@@ -1,6 +1,6 @@
 // import React, { useSearchParams } from "react";
-// import TextField from "@mui/material/TextField";
-// import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
 const FormSerch = (props) => {
   // const [searchParams, setSearchParams] = useSearchParams();
   // console.log(searchParams);
@@ -19,44 +19,43 @@ const FormSerch = (props) => {
   // //   };
   // //   console.log(inputQuery);
   // return (
-  //   <div>
-  //     {/* // <form className="form" onSubmit={handleSubmit}> */}
-  //     {/* // <form className="form" onSubmit={handleSubmit}> */}
-  //     {/* <Box
-  //       sx={{
-  //         display: "flex",
-  //         alignItems: "center",
-  //         justifyContent: "center",
-  //       }}
-  //     >
-  //       <TextField
-  //         //  className="input"
-  //         type="text"
-  //         //  autocomplete="off"
-  //         //  autofocus
-  //         //  placeholder="Search images and photos"
-  //         name="title"
-  //         size="small"
-  //         sx={{ m: 1, width: "35ch" }}
-  //         style={{ backgroundColor: "white" }}
-  //         className="form-control"
-  //         onChange={handleInputQuery}
-  //         //   value={inputQuery}
-  //         id="input-with-sx"
-  //         label="Search movies"
-  //         variant="outlined"
-  //         margin="dense"
-  //       />
-  //     </Box> */}
-  //     <button onClick={() => setSearchParams({ c: "HELLO" })}>Button</button>
-  //     <input
-  //       type="email"
-  //       class="form-control"
-  //       id="exampleInputEmail1"
-  //       aria-describedby="emailHelp"
-  //     ></input>
-  //     {/* </form> */}
-  //   </div>
-  // );
+  <div>
+    <form className="form" onSubmit={handleSubmit}>
+      {/* // <form className="form" onSubmit={handleSubmit}> */}
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <TextField
+          //  className="input"
+          type="text"
+          //  autocomplete="off"
+          //  autofocus
+          //  placeholder="Search images and photos"
+          name="title"
+          size="small"
+          sx={{ m: 1, width: "35ch" }}
+          style={{ backgroundColor: "white" }}
+          className="form-control"
+          onChange={handleInputQuery}
+          //   value={inputQuery}
+          id="input-with-sx"
+          label="Search movies"
+          variant="outlined"
+          margin="dense"
+        />
+      </Box>
+      <button onClick={() => setSearchParams({ c: "HELLO" })}>Button</button>
+      {/* <input
+        type="email"
+        class="form-control"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+      ></input> */}
+    </form>
+  </div>;
 };
 export default FormSerch;
