d413f84fbadb5180fea0185e7447fddb3bacad79
Artem <a.g.sulimko@gmail.com>
2023-12-24 10:35:07 +0100

    s

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -32,35 +32,37 @@ const Cast = () => {
     <div className={css.divCast}>
       {/* <div>++++Cast++++</div> */}
       {castMovies.map((castMovie, index) => {
-        if (castMovie.profile_path) {
+        if (castMovie) {
           return (
             <div className={css.cardCast} key={index}>
               <img
                 className="center-block img-responsive"
                 width="150px"
                 // height="320px"
-                src={`https://image.tmdb.org/t/p/w500/${castMovie.profile_path}`}
+                src={castMovie.profile_path ?`https://image.tmdb.org/t/p/w500/${castMovie.profile_path}` :defaultImage}
                 alt={castMovie.name}
               />
               <p className={css.text}>{castMovie.name}</p>
               <p className={css.text}>{`Character: ${castMovie.character}`}</p>
             </div>
           );
-        } else {
-          return (
-            <div className={css.cardCast} key={index}>
-              <img
-                className="center-block img-responsive defaultImage"
-                width="150px"
-                height="225px"
-                src={defaultImage}
-                alt={castMovie.name}
-              />
-              <p className={css.text}>{castMovie.name}</p>
-              <p className={css.text}>{`Character: ${castMovie.character}`}</p>
-            </div>
-          );
-        }
+        } 
+      //   else {
+        //   return (
+        //     <div className={css.cardCast} key={index}>
+        //       <img
+        //         className="center-block img-responsive defaultImage"
+        //         width="150px"
+        //         height="225px"
+        //         src={defaultImage}
+        //         alt={castMovie.name}
+        //       />
+        //       <p className={css.text}>{castMovie.name}</p>
+        //       <p className={css.text}>{`Character: ${castMovie.character}`}</p>
+        //     </div>
+        //   );
+        // }
+        return null;
       })}
     </div>
   );
